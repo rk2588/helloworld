4f010614608934c4d976a3dbbae237cf676ec4d5
Ryan Kim <ryan.kim@student.csulb.edu>
2023-02-09 16:52:04 -0800

    final ex1 commit

--- js/ex1.js
+++ js/ex1.js
@@ -8,7 +8,7 @@ else if ((number < 0) || (number > 100))
 {
   console.log(`Sorry, ${number} is not a valid entry.`);
 }
-else if isNaN(number)
+else if (isNaN(number))
 {
   console.log(`Invalid entry, not a numerical value.`)
 }
