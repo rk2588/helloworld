4e674557f79b964572f2c99fa815b06d2417c03e
Ryan Kim <ryan.kim@student.csulb.edu>
2023-02-09 16:43:22 -0800

    trying this

--- js/ex1.js
+++ js/ex1.js
@@ -1,15 +1,12 @@
-<html>
-<body>
-  <script>
-  const number = Number(prompt("Enter a number from 1-100:"));
-  alert(${number});
-  if ((number >= 0) && (number <= 100)) {
-    console.log(`Thank you! You entered ${number}, a valid number.`);
-  }
-  else if ((number < 0) || (number > 100)) {
-  console.log(`Sorry, ${number} is not a valid entry.`);
-  }
-    </script> 
-</body>
-</html>
+const number = Number(prompt("Enter a number from 1-100:"));
+alert(${number});
+if ((number >= 0) && (number <= 100)) 
+{
+  console.log(`Thank you! You entered ${number}, a valid number.`);
+}
+else if ((number < 0) || (number > 100)) 
+{
+console.log(`Sorry, ${number} is not a valid entry.`);
+}
+
 
