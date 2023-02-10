1c6f8bc7e47ce1cd4e3797338f70f372a590d819
Ryan Kim <ryan.kim@student.csulb.edu>
2023-02-09 16:19:44 -0800

    execute javascript

--- js/ex1.js
+++ js/ex1.js
@@ -1,8 +1,20 @@
-const number = Number(prompt("Enter a number from 1-100:"));
-alert(${number});
+<html>
+<head>
+  <title>ex1</title>
+</head>
+
+<body>
+  <script>
+  const number = Number(prompt("Enter a number from 1-100:"));
+  alert(${number});
 if ((number >= 0) && (number <= 100)) {
   console.log(`Thank you! You entered ${number}, a valid number.`);
 }
 else if ((number < 0) || (number > 100)) {
   console.log(`Sorry, ${number} is not a valid entry.`);
 }
+    </script> 
+  
+</body>
+</html>
+
