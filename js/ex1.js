52d207e20b6b9e4ae49ce3b52fa88577bc41a26f
Ryan Kim <ryan.kim@student.csulb.edu>
2023-02-09 16:15:28 -0800

    added alert

--- js/ex1.js
+++ js/ex1.js
@@ -1,4 +1,5 @@
 const number = Number(prompt("Enter a number from 1-100:"));
+alert(${number});
 if ((number >= 0) && (number <= 100)) {
   console.log(`Thank you! You entered ${number}, a valid number.`);
 }
