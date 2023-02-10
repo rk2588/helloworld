a50a3d5f5ac565403d8e40e9204eb28a2a25c087
Ryan Kim <ryan.kim@student.csulb.edu>
2023-02-09 16:32:29 -0800

    changes

--- js/ex1.js
+++ js/ex1.js
@@ -1,20 +1,15 @@
 <html>
-<head>
-  <title>ex1</title>
-</head>
-
 <body>
   <script>
   const number = Number(prompt("Enter a number from 1-100:"));
   alert(${number});
-if ((number >= 0) && (number <= 100)) {
-  console.log(`Thank you! You entered ${number}, a valid number.`);
-}
-else if ((number < 0) || (number > 100)) {
+  if ((number >= 0) && (number <= 100)) {
+    console.log(`Thank you! You entered ${number}, a valid number.`);
+  }
+  else if ((number < 0) || (number > 100)) {
   console.log(`Sorry, ${number} is not a valid entry.`);
-}
+  }
     </script> 
-  
 </body>
 </html>
 
